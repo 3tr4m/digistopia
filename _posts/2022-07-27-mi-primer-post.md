ea803d0f6274904522db6b3743d8f4e082da4ef3
Michael Ruiz <github.s69tl@slmail.me>
2022-07-27 15:32:23 -0500

    Configurando SO-SImple

---
+++ _posts/2022-07-27-mi-primer-post.md
@@ -0,0 +1,24 @@
+---
+layout: post
+title:  "Mi primer post"
+date:   2022-07-27 15:11:13 -0500
+categories: jekyll update
+---
+
+# La muerte del dibalo
+
+Estas son las cosas más comunes que les suceden a las personas
+
+
+```shell
+sudo dpkg -l
+```
+
+```ad-note
+title: reminder
+mi recordatorio
+```
+
+>[!info]- Algo de elegancia
+>La elegancia no existe
+
---
+++ assets/js/search-data.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Mi cambio. You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/Mr.Roblog/jekyll/update/2022/07/27/welcome-to-jekyll.html"
+      },{
+        "title": "Mi primer post",
+        "excerpt":"La muerte del dibalo Estas son las cosas más comunes que les suceden a las personas sudo dpkg -ltitle: remindermi recordatorio  [!info]- Algo de eleganciaLa elegancia no existe ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/Mr.Roblog/jekyll/update/2022/07/27/mi-primer-post.html"
+      }]
